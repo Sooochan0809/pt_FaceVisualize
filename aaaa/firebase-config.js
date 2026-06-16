88919418c83b30d6e148837040becfb534cf9cf8
城戸双汰朗 <Sooochan2001@gmail.com>
2026-06-16 11:46:55 +0900

    調整．

---
+++ aaaa/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDR0O3RHxihP81XoDzrjrxYIo4vk0Pf0ak",
+    authDomain: "facetime-8464f.firebaseapp.com",
+    projectId: "facetime-8464f",
+    storageBucket: "facetime-8464f.firebasestorage.app",
+    messagingSenderId: "757982853917",
+    appId: "1:757982853917:web:db95be305ba90471703ff4",
+    measurementId: "G-XG1VEQR7R5"
+};
